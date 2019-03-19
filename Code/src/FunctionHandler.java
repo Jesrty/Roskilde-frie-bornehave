04afb5cebfd70895449fcef119ff4513fb83e58d
aKentorp <43035590+aKentorp@users.noreply.github.com>
2019-03-19 12:35:04 +0100

    With login method

--- Code/src/FunctionHandler.java
+++ Code/src/FunctionHandler.java
@@ -255,4 +255,54 @@ public class FunctionHandler {
 			System.out.println(e);
 		}
 	}
+
+	public int login(){
+		Scanner input = new Scanner(System.in);
+		String username;
+		String password = "";
+		String passwordInput;
+		boolean foundUser = false;
+		boolean admin = false;
+		int toReturn = 3;
+
+
+		System.out.println("Enter Username: ");
+		username = input.next();
+
+		for(Employee employee: employeeList){
+			if (username.equalsIgnoreCase(employee.getUserName())){
+				password = employee.getPassword();
+				foundUser = true;
+				if (employee.isAdmin() == true){
+					admin = true;
+				}
+			}
+		}
+		if (foundUser == false){
+			System.out.println("No user found");
+		}
+		else{
+			System.out.println("Enter password: ");
+			passwordInput = input.next();
+			if(passwordInput.equals(password)){
+				if (admin == true){
+					toReturn = 0;
+				}
+				else {
+					toReturn = 1;
+				}
+			}
+			else{
+				System.out.println("Password does not match the username!!");
+			}
+		}
+
+		return toReturn;
+
+	}
+
+
+	public void getWaitingList(){
+
+	}
 }
