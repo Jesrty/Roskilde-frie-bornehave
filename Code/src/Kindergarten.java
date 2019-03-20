6f3db59940d276f625841a31c72b7783cfe8e585
Bryde ツ <6480864+MBryde@users.noreply.github.com>
2019-03-20 10:14:29 +0100

    updates

--- Code/src/Kindergarten.java
+++ Code/src/Kindergarten.java
@@ -21,17 +21,5 @@ public class Kindergarten extends FunctionHandler{
         for(int i = 0; i < childList.size(); i++){
             System.out.println(Arrays.toString(childList.get(i).toString("save").split(",")));
         }
-<<<<<<< HEAD
-        System.out.println(childList.size());
-        for(int i = 0; employeeList.size() > i; i++){
-            //System.out.println(employeeList.get(i).getUserName());
-        }
-
-<<<<<<< HEAD
-=======
-        //Menu m = new Menu();
->>>>>>> develop
-=======
->>>>>>> c0b3b99f34b8e06c1e12eb3d2d5934b9c1a31ab2
     }
 }
